cbbdbe8ac9ebdbf701d73e051f578c6722ff3751
DiazGallego93 <ediazgallego93@gmail.com>
2018-07-27 10:44:25 -0400

    restaurant model/requirements updated

--- kuri/models.py
+++ kuri/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100, blank=True, default='')
+    address = models.CharField(max_length=100, blank=True, default='')
+    upload = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+
+    def __str__(self):
+        return "{}".format(self.name)
