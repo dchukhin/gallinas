e28a7d3e20681ecf02aaf808ff8689f9d8ada6c8
Dmitriy Chukhin <dchukhin@gmail.com>
2018-07-27 14:30:40 -0400

    add a template for the React app to live in

--- gallinas/urls.py
+++ gallinas/urls.py
@@ -20,6 +20,7 @@ from django.contrib import admin
 
 
 urlpatterns = [
+    url(r'', include('kuri.urls')),
     url(r'^api/', include('api.urls')),
     url(r'^admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
