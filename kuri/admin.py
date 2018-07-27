7275ec410f5602356421d99ed1d6be6b62aeb4a3
DiazGallego93 <ediazgallego93@gmail.com>
2018-07-27 11:51:20 -0400

    kuri admin

--- kuri/admin.py
+++ kuri/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from kuri.models import (
+    Restaurant,
+)
 
 # Register your models here.
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    pass
--- kuri/models.py
+++ kuri/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Restaurant(models.Model):
-    name = models.CharField(max_length=100, blank=True, default='')
+    name = models.CharField(max_length=100, blank=True, default='', unique=True)
     address = models.CharField(max_length=100, blank=True, default='')
+    city = models.CharField(max_length=100, blank=True, default='')
+    state = models.CharField(max_length=100, blank=True, default='')
+    zip_code = models.CharField(max_length=10, blank=True, null=True)
     upload = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
 
     def __str__(self):
